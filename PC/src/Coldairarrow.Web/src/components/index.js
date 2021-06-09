a0031acaeb2bd8d026ec4c880d341781b70b2d0b
bread <ismatch@qq.com>
2021-06-09 21:50:34 +0800

    delete files

--- PC/src/Coldairarrow.Web/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import MultiTab from '@/components/MultiTab'
-import IconSelector from '@/components/IconSelector'
-import ExceptionPage from '@/components/Exception'
-
-export {
-  MultiTab,
-  ExceptionPage,
-  IconSelector
-}
